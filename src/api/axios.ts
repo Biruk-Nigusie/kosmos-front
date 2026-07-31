7107be58b80d6a837b0bd693ba3d6af20d3160e9
biruk11011@gmail.com <biruknigusie00@gmail.com>
2026-07-31 16:31:51 +0300

    base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://kosmos-core.onrender.com",
+  baseURL: "https://kosmos-core-1.onrender.com",
   withCredentials: true,
 });
 
